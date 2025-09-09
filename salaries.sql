292dbbf8a3ca8d2232fb85451986907a69bdb22f
MariiaSam <kontragent@ukr.net>
2025-09-09 21:36:02 +0300

    add

--- salaries.sql
+++ salaries.sql
@@ -38,3 +38,72 @@ SELECT
  ORDER BY salary_in_usd  DESC 
  
  LIMIT 5;
+
+ SELECT
+	1 AS nmb_1
+	,1 AS nmb_2
+	,5+5 AS nmb_3;
+
+SELECT *
+FROM salaries
+LIMIT 10;
+	
+SELECT
+	COUNT(*) AS cnt_all
+	, COUNT(exp_level) AS cnt_all
+	FROM salaries
+LIMIT 10;
+
+
+SELECT
+	COUNT(*) AS cnt_all
+	, COUNT(exp_level) AS cnt_all
+	, COUNT(DISTINCT exp_level)
+	FROM salaries
+LIMIT 10;
+
+-- описова статистика
+-- середян ЗП 
+ 
+SELECT
+	AVG(salary_in_usd) AS salary_avg
+	, MIN(salary_in_usd)  AS salary_min
+	, MAX(salary_in_usd)  AS salary_max
+	FROM salaries 
+LIMIT 10;
+
+SELECT
+	AVG(salary_in_usd) AS salary_avg
+	, MIN(salary_in_usd)  AS salary_min
+	, MAX(salary_in_usd)  AS salary_max
+	FROM salaries 
+	WHERE year = 2023
+	
+LIMIT 10;
+
+
+SELECT
+	"year" 
+	, exp_level
+	, salary_in_usd
+	, salary_in_usd * 41 AS salary_in_uah
+FROM salaries 
+LIMIT 10;
+
+SELECT
+	"year" 
+	, exp_level
+	, salary_in_usd
+	, salary_in_usd * 41 AS salary_in_uah
+-- 'SE' - Senior
+-- 'MI' - Middle
+-- Other
+	, CASE 
+		WHEN exp_level = 'SE'
+		THEN 'Senior'
+		WHEN exp_level = 'MI'
+		THEN 'Middle'
+		ELSE 'Other'END AS full_level
+	
+FROM salaries 
+LIMIT 100
