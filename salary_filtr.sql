6232382917203a3233d4227095979006d7537bf2
MariiaSam <kontragent@ukr.net>
2025-09-15 23:24:31 +0300

    add

--- salary_filtr.sql
+++ salary_filtr.sql
@@ -29,3 +29,47 @@ WHERE
 		OR exp_level != 'SE')
 LIMIT 20;
 
+
+SELECT *
+FROM salaries
+WHERE 1=1
+AND year = 2020
+LIMIT 20;
+
+
+SELECT DISTINCT year
+FROM salaries
+WHERE 1=1
+	AND year > 2020
+	AND year < 2023
+LIMIT 20;
+
+
+SELECT DISTINCT year
+FROM salaries
+WHERE 1=1
+	AND year NOT BETWEEN 2021 and 2023
+--	AND year >= 2020
+--	AND year <= 2023
+LIMIT 20;
+
+
+
+SELECT DISTINCT year
+FROM salaries
+WHERE 1=1
+	AND year IN (2020, 2021)
+--	AND year >= 2020
+--	AND year <= 2023
+LIMIT 20;
+
+
+
+SELECT DISTINCT job_title 
+FROM salaries
+WHERE 1=1
+	AND job_title  IN ('Data Analysis', 'Data Science')
+--	AND year >= 2020
+--	AND year <= 2023
+LIMIT 20;
+
