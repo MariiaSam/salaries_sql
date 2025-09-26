b2c87d7baff7127c323ed36e1c27394b91221656
MariiaSam <kontragent@ukr.net>
2025-09-26 17:31:24 +0300

    add

--- 19.09.sqlite/join.sql
+++ 19.09.sqlite/join.sql
@@ -4,6 +4,14 @@ SELECT * FROM projects;
 SELECT * FROM project_assignments;
 SELECT * FROM test;
 
+SELECT * FROM customers c;
+SELECT * FROM orders o;
+SELECT * FROM order_items oi;
+SELECT * FROM shipments s;
+SELECT * FROM  payments p;
+SELECT * FROM returns r ;
+
+
 
 SELECT *
 FROM project_assignments
@@ -17,3 +25,19 @@ SELECT
 	, projects.role
 FROM project_assignments
 JOIN projects ON project_assignments.project_id  = projects.project_id;
+
+SELECT 
+	o.order_date 
+	, oi.product_name
+	, oi.quantity
+FROM orders o
+JOIN order_items oi ON oi.order_id = o.id;
+
+
+SELECT 
+	oi.product_name
+	, COUNT(o.id)
+FROM orders o
+JOIN order_items oi ON oi.order_id = o.id;
+WHERE  oi.product_name LIKE 'B'
+GROUP BY oi.product_name;
